20857e13f4b6c952eb2ad1f8f6a615132e870efd
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-05 12:43:47 -0700

    added users table

---
+++ src/main/java/com/example/javaapi2/models/Users.java
@@ -0,0 +1,50 @@
+package com.example.javaapi2.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+   public Users(Users copy) {
+       id = copy.id;
+       username = copy.username;
+       email = copy.email;
+   }
+
+   public Users() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
